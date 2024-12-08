737da5b817eb004768a09b5af3b8848b94c8576b
unknown <mynamerahulkumar13@gmail.com>
2024-12-08 20:17:07 +0000

    updated next files

--- foodies-startup-project-nextjs/app/components/main-header.tsx
+++ foodies-startup-project-nextjs/app/components/main-header.tsx
@@ -1,13 +1,15 @@
 import Link from "next/link";
 import logoImg from '../../assets/logo.png';
+import classes from './main-header.module.css';
+import Image from "next/image";
 export default function MainHeader(){
     return(
-        <header>
-            <Link href="/">
-            <img src={logoImg.src} alt="A food plate" />
+        <header className={classes.header}>
+            <Link href="/" className={classes.logo}>
+            <Image src={logoImg} alt="A food plate" />
             Next level food
             </Link>
-            <nav>
+            <nav className={classes.nav}>
                 <ul>
                     <li>
                         <Link href="/meals">Meals</Link>
